4fa573e41ae4e5df68d9ca1191931b83c2c24e95
CoolLoong <wingdon@foxmail.com>
2025-07-20 14:38:40 +0800

    feat: update 1.21.8

---
+++ platforms/bukkit/nms/v1_21_8/build.gradle.kts
@@ -0,0 +1,25 @@
+import io.papermc.paperweight.userdev.ReobfArtifactConfiguration
+
+plugins {
+    id("io.papermc.paperweight.userdev")
+}
+
+paperweight.reobfArtifactConfiguration = ReobfArtifactConfiguration.MOJANG_PRODUCTION
+
+repositories {
+    mavenLocal()
+    maven {
+        name = "AliYun-Snapshot"
+        url = uri("https://packages.aliyun.com/maven/repository/2421751-snapshot-i7Aufp/")
+        credentials {
+            username = project.findProperty("aliyun.package.user") as String? ?: System.getenv("ALY_USER")
+            password = project.findProperty("aliyun.package.password") as String? ?: System.getenv("ALY_PASSWORD")
+        }
+    }
+}
+
+dependencies {
+    api(project(":platforms:bukkit:common"))
+    paperweight.devBundle("com.starsrealm.nylon", "1.21.8-R0.1-SNAPSHOT")
+    implementation("xyz.jpenilla", "reflection-remapper", Versions.Bukkit.reflectionRemapper)
+}
